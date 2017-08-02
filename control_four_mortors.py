4997a9d6cf368ef698eb1265f641903949312976
Yasuyuki Fujii <is0232xf@gmail.com>
2017-08-02 15:03:41 +0900

    PWMを使わないように変更した

--- control_four_mortors.py
+++ control_four_mortors.py
@@ -10,24 +10,26 @@ import RPi.GPIO as GPIO
 
 
 # モーターを2秒間回転させる
-def rotate_motor(mortor_channel, pwm):
-    pwm.start(0)
+def rotate_motor(mortor_channel):
     print 'rotation'
     GPIO.output(mortor_channel[0], 1)
     GPIO.output(mortor_channel[1], 0)
 
-    print 'Duty Cycle is 50'
-    pwm.ChangeDutyCycle(50)
     time.sleep(2)
 
 
 # モーターの回転を止める
-def break_rotation(mortor_channel, pwm):
+def break_rotation(mortor_channel):
     print 'break'
-    pwm.stop()
+    GPIO.output(mortor_channel[0], 1)
+    GPIO.output(mortor_channel[1], 1)
+    time.sleep(1)
+
+
+def cleanup_GPIO(mortor_channel):
     GPIO.output(mortor_channel[0], 0)
     GPIO.output(mortor_channel[1], 0)
-    GPIO.cleanup(mortor_channel)
+
     time.sleep(1)
 
 
@@ -42,40 +44,55 @@ if __name__ == "__main__":
     # 1つ目のモーターのチャンネル設定
     out_channel1 = 16
     out_channel2 = 18
-    pwm_channel1 = GPIO.PWM(12, 0)
+    # pwm_channel1 = 12
     mortor_channels1 = [out_channel1, out_channel2]
 
     # 2つ目のモーターのチャンネル設定
     out_channel3 = 24
     out_channel4 = 26
-    pwm_channel2 = GPIO.PWM(22, 0)
+    pwm_channel2 = 22
     mortor_channels2 = [out_channel3, out_channel4]
 
     # 3つ目のモーターのチャンネル設定
     out_channel5 = 7
     out_channel6 = 11
-    pwm_channel3 = GPIO.PWM(3, 0)
+    pwm_channel3 = 3
     mortor_channels3 = [out_channel5, out_channel6]
 
     # 4つ目のモーターのチャンネル設定
     out_channel7 = 33
-    out_channel8 = 35
-    pwm_channel4 = GPIO.PWM(31, 0)
+    out_channel8 = 37
+    pwm_channel4 = 12
     mortor_channels4 = [out_channel7, out_channel8]
 
-
+    # 各ピンのGPIO設定
     GPIO.setup(mortor_channels1, GPIO.OUT)
-    GPIO.setup(mortor_channels2, GPIO.OUT)
-    GPIO.setup(mortor_channels3, GPIO.OUT)
-    GPIO.setup(mortor_channels4, GPIO.OUT)
+    # pwm1 = GPIO.PWM(mortor_channels1[2], dc)
 
-    rotate_motor(mortor_channels1, pwm_channel1)
-    break_rotation(mortor_channels1, pwm_channel1)
-    rotate_motor(mortor_channels2, pwm_channel2)
-    break_rotation(mortor_channels2, pwm_channel2)
-    rotate_motor(mortor_channels3, pwm_channel3)
-    break_rotation(mortor_channels3, pwm_channel3)
-    rotate_motor(mortor_channels4, pwm_channel4)
-    break_rotation(mortor_channels4, pwm_channel4)
+    GPIO.setup(mortor_channels2, GPIO.OUT)
+    # pwm2 = GPIO.PWM(mortor_channels1[2], dc)
+
+    GPIO.setup(mortor_channels3, GPIO.OUT)
+    # pwm3 = GPIO.PWM(mortor_channels1[2], dc)
+
+    GPIO.setup(mortor_channels4, GPIO.OUT)
+    # pwm4 = GPIO.PWM(mortor_channels1[2], dc)
+
+    # モータの回転と停止（制御部分）
+    rotate_motor(mortor_channels1)
+    rotate_motor(mortor_channels2)
+    rotate_motor(mortor_channels3)
+    rotate_motor(mortor_channels4)
+
+    break_rotation(mortor_channels1)
+    cleanup_GPIO(mortor_channels1)
+    break_rotation(mortor_channels2)
+    cleanup_GPIO(mortor_channels2)
+    break_rotation(mortor_channels3)
+    cleanup_GPIO(mortor_channels3)
+    break_rotation(mortor_channels4)
+    cleanup_GPIO(mortor_channels4)
+
+    GPIO.cleanup()
 
     print 'finish'
